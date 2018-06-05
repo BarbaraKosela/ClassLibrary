95579595953673be4c29ddb6ee2f1f6f83dab556
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-05 16:58:15 -0300

    Adição de repositório Class Library
    Aqui aprendemos a como separar as classes e os WFA

---
+++ BaseProvinha/Modelo/Aluno.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Aluno
+    {
+
+        private string Nome;
+        private int Idade;
+        private string Turno;
+        private string Turma;
+        private int Matricula;
+        private List<double> Notas = new List<double>();
+
+
+        public void SetNome(string nome)
+        {
+            if (nome.Trim().Count() < 3)
+            {
+                throw new Exception("Nome deve conter no mínimo 3 caracteres");
+            }
+            
+            if (nome.Trim().Count() > 100)
+            {
+                throw new Exception("Nome deve conter no máximo 100 caracteres");
+            }
+
+            
+            Nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public void SetIdade(int idade)
+        {
+
+            if (idade < 4)
+            {
+                throw new Exception("Idade deve ser no mínimo maior do que 4 anos");
+            }
+
+            if (idade > 23)
+            {
+                throw new Exception("Idade deve ser menor do que 23 anos");
+            }
+            Idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return Idade;
+        }
+
+
+        public void SetTurno(string turno)
+        {
+            if (turno.Trim()== "")
+            {
+                throw new Exception("Seu turno deve conter no mínimo 7 caracteres");
+            }
+
+            if (turno.Trim() != "Matutino" || turno.Trim() != "Vespertino" || turno.Trim() != "Noturno")
+            {
+                throw new Exception("Turno deve conter somente matutino, vespertino ou noturno");
+            }
+            
+               
+                Turno = turno;
+
+        }
+
+        public string GetTurno()
+        {
+            return Turno;
+        }
+
+        public void SetTurma(string turma)
+        {
+            if (turma.Trim().Count() < 1)
+            {
+                throw new Exception("A turma deve ser maior do que 1");
+            }
+            Turma = turma;
+        }
+
+        public void SetMatricula(int matricula)
+        {
+            if (matricula < 0)
+            {
+                throw new Exception("A matricula deve ser maior do que 0")
+            }
+        }
+    }
+}
