9269948c616b7866984706346f8df72dab7dc812
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-08 14:44:59 -0300

    Inicialização de Serializable

---
+++ BaseProvinha/ExemploSerializacao/ExemploSerializacao/ListaPersonagem.cs
@@ -0,0 +1,35 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploSerializacao
+{
+    public partial class ListaPersonagem : Form
+    {
+        public ListaPersonagem()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Personagem personagem = new Personagem();
+            personagem.SetNome(txtNome.Text);
+            personagem.SetCla(cbCla.SelectedItem.ToString());
+            personagem.SetNivelChakra(Convert.ToInt32(txtNivelChakra.Text));
+        }
+    }
+}
---
+++ BaseProvinha/ExemploSerializacao/ExemploSerializacao/Tudo.cs
@@ -0,0 +1,25 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSerializacao
+{
+    [Serializable]
+    class Tudo
+    {
+        List<Personagem> personagens = new List<Personagem>();
+
+        public void AdicionarPersonagem(Personagem personagem)
+        {
+            personagens.Add(personagem);
+        }
+
+        public List<Personagem> ObterPersonagens()
+        {
+            return personagens;
+        }
+    }
+}
