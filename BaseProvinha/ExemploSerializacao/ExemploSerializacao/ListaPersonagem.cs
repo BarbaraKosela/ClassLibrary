da9f99603a0a7f9a05f44b94ff3f6ca48811c90d
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-08 14:50:11 -0300

    BinaryFormatter

--- BaseProvinha/ExemploSerializacao/ExemploSerializacao/ListaPersonagem.cs
+++ BaseProvinha/ExemploSerializacao/ExemploSerializacao/ListaPersonagem.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -28,8 +30,16 @@ namespace ExemploSerializacao
         {
             Personagem personagem = new Personagem();
             personagem.SetNome(txtNome.Text);
-            personagem.SetCla(cbCla.SelectedItem.ToString());
             personagem.SetNivelChakra(Convert.ToInt32(txtNivelChakra.Text));
+            personagem.SetCla(cbCla.SelectedItem.ToString());
+
+
+            Tudo tudo = new Tudo();
+            tudo.AdicionarPersonagem(personagem);
+
+            BinaryFormatter binaryWritter = new BinaryFormatter();
+            Stream stream = new FileStream("Personagem.bin", FileMode.Create, FileAccess.Write);
+            binaryWritter.Serialize(stream, tudo);
         }
     }
 }
